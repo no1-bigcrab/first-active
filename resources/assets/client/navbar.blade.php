268d398fcf58a2cab18cffc7013478438e1d28e6
MacOS <macos@MacBook-Pro-cua-MacOS.local>
2023-04-19 21:50:56 +0700

    blade test

---
+++ resources/assets/client/navbar.blade.php
@@ -0,0 +1,16 @@
+<div class="container-fluid mb-5">
+    <div class="row border-top px-xl-5">
+        <div class="col-lg-3 d-none d-lg-block">
+            <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100"
+                data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
+                <h6 class="m-0">Categories</h6>
+                <i class="fa fa-angle-down text-dark"></i>
+            </a>
+            <?php include('./menu-left')?>
+        </div>
+        <div class="col-lg-9">
+            <?php include('./menu-nav')?>
+            <?php include('./menu-slideshow') ?>
+        </div>
+    </div>
+</div>
---
+++ resources/views/client/home/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('./../assets/head')?>
+
+<body>
+    <div>Hello World</div>
+</body>
+
+</html>
